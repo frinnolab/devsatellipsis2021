e66c4b208a11c13d866ac7dfde0a5caadfcb426c
Frank Leons <frinno.frasha@gmail.com>
2021-04-23 12:39:48 +0300

    Session 2 Commint

---
+++ Session2App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Session2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DOPE session 2!");
+        }
+    }
+}
