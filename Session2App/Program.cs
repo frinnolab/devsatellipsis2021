decf39a3313e42b7a056432a28f86b1383cb6ff2
Frank Leons <frinno.frasha@gmail.com>
2021-04-26 20:52:06 +0300

    Session2 starter Code updated.

--- Session2App/Program.cs
+++ Session2App/Program.cs
@@ -7,6 +7,20 @@ namespace Session2App
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to DOPE session 2!");
+
+            //Data Types
+            string Fullname = "Jooma Dooma"; // String Data type
+
+            int birthYear = 2001; //int Data type.
+
+            decimal max_height = 100.5M; //decimal Data type.
+
+            float _amount = 250.5f; //float data type.
+
+            bool isLoggedIn = false; // boolean Data type.
+
+            Console.WriteLine(Fullname);
+
         }
     }
 }
