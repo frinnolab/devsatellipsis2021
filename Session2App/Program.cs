2313b20319a59775bf4ceac97285733e8d1dc4c4
naomi nilla <nillamomy@gmail>
2021-04-28 21:59:59 +0300

    Update Program.cs

--- Session2App/Program.cs
+++ Session2App/Program.cs
@@ -7,6 +7,14 @@ namespace Session2App
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to DOPE session 2!");
+            Console.WriteLine("Enter  username");
+            string username =  Console.Readline();
+            Console.WriteLine("username is:" + username);
+
+            console.WriteLine("Enter your age");
+            int  age = convert.ToInt32(console.Readline());
+            console.WriteLine("Your  age is:"+ age);
+            console.WriteLine("Account with  username:"+username,"age:"+age,"successfully  created");
         }
     }
 }
