280111c8b94e946bb567cb7aa0ed10a8aab99007
sophia-99 <sophiamemba@gmail.com>
2021-04-29 16:27:02 +0300

    code changes

---
+++ devsatellipsis2021-session2/README.md
@@ -0,0 +1,2 @@
+# DEVELOPERS AT ELLIPSIS 2021
+Repository for the "Developer Outreach Program at Ellipsis Digital Company" ~ "DOP@E21".
---
+++ devsatellipsis2021-session2/Session2App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Session2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your UserName :");
+String user_name=Console.ReadLine();
+Console.WriteLine("Enter your Age :");
+int age=System.Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("age is " + age);
+Console.WriteLine("Account with UserName : " + user_name + " and Age " + age + " is successfully created!");
+        }
+    }
+}
