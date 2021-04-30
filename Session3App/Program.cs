af705e22885ba0745896f4d3752b947eaaa105a6
Frank Leons <frinno.frasha@gmail.com>
2021-04-30 14:15:46 +0300

    Session Code

--- Session3App/Program.cs
+++ Session3App/Program.cs
@@ -12,49 +12,78 @@ namespace Session3App
             #region Arrays
             
             //DataType [] name = new DataType [length]
-            int [] ages = new int [4];
-            ages[0] = 67;
-            ages[1] = 15;
-            ages[2] = 31;
-            ages[3] = 78;
+
+            #region Int Array
+            // int [] ages = new int [4];
+            // ages[0] = 67;
+            // ages[1] = 15;
+            // ages[2] = 31;
+            // ages[3] = 78;
+            //Console.WriteLine("Age: "+ ages[0]);
+             #endregion
+            
             
             //String Arrays
-            string [] locations = new string[3];
-            locations[0] = "Dar es salaam";
-            locations[1] = "Nairobi";
-            locations[2] = "Joburg";
+            #region String Array
+            // string [] locations = new string[3];
+            // locations[0] = "Dar es salaam";
+            // locations[1] = "Nairobi";
+            // locations[2] = "Joburg";
 
-            //Double arrays
-            double[] balance = new double[3]{100.4, 253.4, 17.64};
+            //string oddCity = locations[3];
 
-            string oddCity = locations[2];
+            //Console.WriteLine(oddCity);
             #endregion
 
             #region Conditionals
-            
+
+            #region If/Else
             //Conditionals
+            #region 
             //If/ Else
-            if(balance[0]!=100.4){
-                balance[0] = 100.5;
-            }else{
-                Console.WriteLine("Balance: "+ balance[0]);
-            }
+            // if(balance[0]!=100.4){
+            //     balance[0] = 100.5;
+            // }else{
+            //     Console.WriteLine("Balance: "+ balance[0]);
+            // }
+            #endregion
+            #endregion
 
-
+            #region Switch
+            #region 
             //Switch
-            switch(locations[0]){
-                case "Dar es salaam":
-                    Console.WriteLine("location: "+ locations[0]);
-                    break;
-                case "Nairobi":
-                    Console.WriteLine("location: "+ locations[0]);
-                    break;
-                default :
-                    Console.WriteLine("Location doesn't exist");
-                    break;
-            }
+            // switch(locations[0]){
+            //     case "Dar es salaam":
+            //         Console.WriteLine("location: "+ locations[0]);
+            //         break;
+            //     case "Nairobi":
+            //         Console.WriteLine("location: "+ locations[0]);
+            //         break;
+            //     default :
+            //         Console.WriteLine("Location doesn't exist");
+            //         break;
+            // }
+            #endregion
+            #endregion
+            #endregion
+
+            #region Iterators
+            #region  for loop
+            // for (int i = 0; i < ages.Length; i++)
+            // {
+            //     if (ages[i]< 18)
+            //     {
+            //         Console.WriteLine("Age :" + ages[i]+", you cant vote");
+            //     }else{
+            //         Console.WriteLine("Age: "+ ages[i]+ ", you can vote.");
+            //     }
+            // }
+
+            //
+            #endregion
             #endregion
            // Console.WriteLine(balance[0]);
+           #endregion
         }
     }
 }
